84c15d2c4fe2537798c9358b100b7120db28cb07
HzB <hzbdaima@139.com>
2018-12-23 12:10:47 +0800

    ff

---
+++ NPUtils/Other/UIPasteboard+YU.h
@@ -0,0 +1,28 @@
+//
+//  UIPasteboard+YU.h
+//  YUKit<https://github.com/c6357/YUKit>
+//
+//  Created by BruceYu on 16/1/5.
+//  Copyright © 2016年 BruceYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+///复制粘贴
+@interface UIPasteboard (YU)
+
++(void)setString:(NSString*)string;
++(void)setStrings:(NSArray*)strings;
+
+
++(void)setImage:(UIImage*)image;
++(void)setImages:(NSArray*)images;
+
+
++(void)setColor:(UIColor*)color;
++(void)setColors:(NSArray*)colors;
+
++(void)setUrl:(NSURL*)url;
++(void)setUrls:(NSArray*)urls;
+
+@end
